3f69b418e935d91b2ee61c71a4471d3043a51895
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-10-07 18:26:28 -0300

    Create exe_variaveis.js

---
+++ secao1/js/exe_variaveis.js
@@ -0,0 +1,7 @@
+const nome = "Agosto";
+let idade = 21;
+const altura = 1.85;
+let peso = 75.0;
+let imc = peso/(altura*altura);
+// console.log(nome, "\ntem: ", idade,"\naltura: ", altura, "\npeso: ", peso, "\nIMC: ", imc);
+console.log(`${nome}\ntem:${idade}\naltura: ${altura}\npeso: ${peso}\nIMC: ${imc}`);
