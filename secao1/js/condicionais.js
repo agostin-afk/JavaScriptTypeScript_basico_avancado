239e0e41811b941dbfa08309923fe7ca3698f40b
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-04-20 09:20:38 -0300

    comparacao

---
+++ secao1/js/condicionais.js
@@ -0,0 +1,11 @@
+nome = 'lucas';
+if (nome == 'pedro'){
+    console.log("Seu nome é pedro");
+}
+else if (nome == "agosto"){
+    console.log("Seu nome é agosto");
+}
+else{
+    console.log("Seu nome não foi cadastrado")
+}
+// operadores condicionais funcionam normalmente (!=, !==, && , ||, !)
